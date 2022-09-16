19ec5e2ba069fa0589bd929872e891b7089f5a3b
Junha9 <ghno20@gmail.com>
2022-09-16 17:22:20 +0900

    0916

--- crud/articles/views.py
+++ crud/articles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Article
+from .forms import ArticleForm
 # Create your views here.
 
 def index(request):
@@ -11,7 +12,11 @@ def index(request):
     return render(request, 'articles/index.html', context)
 
 def new(request):
-    return render(request, 'articles/new.html')
+    form = ArticleForm()
+    context = {
+        'form' : form,
+    }
+    return render(request, 'articles/new.html', context)
 
 def create(request):
     title = request.POST.get('title')
