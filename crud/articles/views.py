e6516bd54b6b656bdc98c9518911d439f3f02ae2
Junha9 <ghno20@gmail.com>
2022-09-19 00:07:24 +0900

    0918

--- crud/articles/views.py
+++ crud/articles/views.py
@@ -11,20 +11,32 @@ def index(request):
     }
     return render(request, 'articles/index.html', context)
 
-def new(request):
-    form = ArticleForm()
+# def new(request):
+#     form = ArticleForm()
+#     context = {
+#         'form' : form,
+#     }
+#     return render(request, 'articles/new.html', context)
+
+def create(request):
+    # title = request.POST.get('title')
+    # content = request.POST.get('content')
+    # article = Article(title=title, content=content)
+    # article.save()
+    # return render(request, 'articles/create.html')
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm()
     context = {
         'form' : form,
     }
-    return render(request, 'articles/new.html', context)
-
-def create(request):
-    title = request.POST.get('title')
-    content = request.POST.get('content')
-    article = Article(title=title, content=content)
-    article.save()
-    # return render(request, 'articles/create.html')
-    return redirect('articles:detail', article.pk)
+    return render(request, 'articles/create.html', context)
+    print(f'error: {form.errors}')
+    return redirect('articles:new')
 
 def detail(request, pk):
     article = Article.objects.get(pk=pk)
@@ -34,22 +46,36 @@ def detail(request, pk):
     return render(request, 'articles/detail.html', context)
 
 def delete(request, pk):
-    if request.method == "POST":
-        article = Article.objects.get(pk=pk)
-        article.delete()
-    return redirect('articles:index')
-
-def edit(request, pk):
     article = Article.objects.get(pk=pk)
-    context = {
-        'article': article,
-    }
-    return render(request, 'articles/edit.html', context)
+    if request.method == "POST":
+        article.delete()
+        return redirect('articles:index')
+    return redirect('articles:detail', article.pk)
+
+# def edit(request, pk):
+#     article = Article.objects.get(pk=pk)
+#     form = ArticleForm(instance=article)
+#     context = {
+#         'article': article,
+#         'form': form,
+#     }
+#     return render(request, 'articles/edit.html', context)
 
 def update(request, pk):
     article = Article.objects.get(pk=pk)
-    article.title = request.POST.get('title')
-    article.content = request.POST.get('content')
-    article.save()
+    # article.title = request.POST.get('title')
+    # article.content = request.POST.get('content')
+    # article.save()
     # return render(request, 'articles/create.html')
-    return redirect('articles:detail', article.pk)
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm(instance=article)
+    context = {
+        'form' : form,
+        'article' : article,
+    }
+    return render(request, 'articles/update.html', context)
--- crud/crud/settings.py
+++ crud/crud/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -33,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # Local apps
     'articles',
+    'accounts',
 
     # Django apps
     'django.contrib.admin',
@@ -89,6 +91,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -128,3 +131,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
